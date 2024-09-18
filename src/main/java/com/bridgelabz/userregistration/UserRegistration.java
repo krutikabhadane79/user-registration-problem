b8215dc5e43b243a2774c4bf748c7a0a48f05807
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-18 22:46:33 +0530

    Added: UC-1 As a User need to enter a valid First Name

---
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.userregistration;
+
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+    public boolean validateFirstName(String fname) {
+        String regex = "[A-Z][a-z]{3,}";
+        Pattern pattern=Pattern.compile(regex);
+        boolean check=pattern.matcher(fname).matches();
+        return check;
+    }
+
+    public static void main(String[] args) {
+        UserRegistration userRegistration=new UserRegistration();
+        System.out.println(userRegistration.validateFirstName("Kruti"));
+    }
+}
