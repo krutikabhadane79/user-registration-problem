06cf162fd532d5d99a964163a9bd42cd2d4e73e5
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-18 22:50:09 +0530

    Added: UC-2 As a User need to enter a valid Last Name

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -10,8 +10,16 @@ public class UserRegistration {
         return check;
     }
 
+    public boolean validateLatName(String lname) {
+        String regex = "[A-Z][a-z]{3,}";
+        Pattern pattern=Pattern.compile(regex);
+        boolean check=pattern.matcher(lname).matches();
+        return check;
+    }
+
     public static void main(String[] args) {
         UserRegistration userRegistration=new UserRegistration();
         System.out.println(userRegistration.validateFirstName("Kruti"));
+        System.out.println(userRegistration.validateLatName("Patil"));
     }
 }
