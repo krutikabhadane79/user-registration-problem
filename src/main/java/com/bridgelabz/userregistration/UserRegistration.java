4d7975edb474a2ed7abd7bd026c9b3225d0fe3b1
Krutika Bhadane <krutikabhadane79@gmail.com>
2024-09-19 23:04:34 +0530

    Refactor: UC-12 throw custom exceptions in case of invalid user details

--- src/main/java/com/bridgelabz/userregistration/UserRegistration.java
+++ src/main/java/com/bridgelabz/userregistration/UserRegistration.java
@@ -1,56 +1,76 @@
 package com.bridgelabz.userregistration;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class UserRegistration {
-    public boolean validateFirstName(String fname) {
+public class UserRegistration  {
+    public boolean validateFirstName(String fname) throws UserRegistrationException {
         String regex = "[A-Z][a-z]{3,}";
         Pattern pattern=Pattern.compile(regex);
         boolean check=pattern.matcher(fname).matches();
-        return check;
+        if (check) {
+            return true;
+        }
+        else {
+            throw new UserRegistrationException("Invalid first name "+fname);
+        }
     }
 
-    public boolean validateLastName(String lname) {
+    public boolean validateLastName(String lname) throws UserRegistrationException {
         String regex = "[A-Z][a-z]{3,}";
         Pattern pattern=Pattern.compile(regex);
         boolean check=pattern.matcher(lname).matches();
-        return check;
+        if (check) {
+            return true;
+        }
+        else {
+            throw new UserRegistrationException("Invalid last name "+lname);
+        }
     }
 
-    public boolean validateEmail(String email) {
+    public boolean validateEmail(String email) throws UserRegistrationException {
         String regex = "[a-zA-Z0-9_.]*[-]*[+]*[a-zA-Z0-9]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+";
         Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher(email);
         boolean check=pattern.matcher(email).matches();
-        return check;
+        if (check) {
+            return true;
+        }
+        else {
+            throw new UserRegistrationException("Invalid email address "+email);
+        }
     }
 
-    public boolean validatePhone(String phone) {
+    public boolean validatePhone(String phone) throws UserRegistrationException {
         String regex = "(0|91)?[-\\s]?[6-9][0-9]{9}";
         Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher(phone);
         boolean check=pattern.matcher(phone).matches();
-        return check;
+        if (check) {
+            return true;
+        }
+        else {
+            throw new UserRegistrationException("Invalid phone number "+phone);
+        }
     }
 
-    public boolean validatePassword(String password) {
+    public boolean validatePassword(String password) throws UserRegistrationException {
         String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&*+=]).{8,}$";
         Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(password);
         boolean check = pattern.matcher(password).matches();
-        return check;
+        if (check) {
+            return true;
+        }
+        else {
+            throw new UserRegistrationException("Invalid entered password "+password);
+        }
     }
 
     public boolean validateAllEmailSamples(String email) {
         String regex = "[a-zA-Z0-9_.]*[-]*[+]*[a-zA-Z0-9]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+";
         Pattern pattern=Pattern.compile(regex);
-        Matcher matcher=pattern.matcher(email);
         boolean check=pattern.matcher(email).matches();
         return check;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UserRegistrationException {
         UserRegistration userRegistration=new UserRegistration();
         System.out.println(userRegistration.validateFirstName("Kruti"));
         System.out.println(userRegistration.validateLastName("Patil"));
--- src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/userregistration/UserRegistrationTest.java
@@ -7,26 +7,26 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class UserRegistrationTest {
     @Test
-    public void givenFirstName_WhenValid_ShouldReturns_True() {
+    public void givenFirstName_WhenValid_ShouldReturns_True() throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         boolean result=user.validateFirstName("Kruti");
         Assert.assertTrue(result);
     }
     @Test
-    public void givenFirstName_WhenValid_ShouldReturns_False() {
+    public void givenFirstName_WhenValid_ShouldReturns_False() throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         boolean result=user.validateFirstName("kruti");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void givenLastName_WhenValid_ShouldReturn_True() {
+    public void givenLastName_WhenValid_ShouldReturn_True() throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         boolean result=user.validateLastName("Patil");
         Assert.assertTrue(result);
     }
     @Test
-    public void givenLastName_WhenValid_ShouldReturn_False() {
+    public void givenLastName_WhenValid_ShouldReturn_False() throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         boolean result=user.validateLastName("patil");
         Assert.assertFalse(result);
@@ -34,39 +34,39 @@ public class UserRegistrationTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"abc@yahoo.com","abc-100@yahoo.com"})
-    public void givenEmail_WhenValid_ShouldReturn_True(String email) {
+    public void givenEmail_WhenValid_ShouldReturn_True(String email) throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         Assert.assertTrue(user.validateEmail(email));
     }
     
     @ParameterizedTest
     @ValueSource(strings = {"abc","abc@.com.my"})
-    public void givenEmail_WhenValid_ShouldReturn_False(String email) {
+    public void givenEmail_WhenValid_ShouldReturn_False(String email) throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         Assert.assertFalse(user.validateEmail(email));
     }
 
     @Test
-    public void givenPhoneNumber_WhenValid_ShouldReturn_True() {
+    public void givenPhoneNumber_WhenValid_ShouldReturn_True() throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         boolean result=user.validatePhone("91 8878564543");
         Assert.assertTrue(result);
     }
     @Test
-    public void givenPhoneNumber_WhenValid_ShouldReturn_False() {
+    public void givenPhoneNumber_WhenValid_ShouldReturn_False() throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         boolean result=user.validatePhone("5436546546");
         Assert.assertFalse(result);
     }
 
     @Test
-    public void givenPassword_WhenValid_ShouldReturn_True() {
+    public void givenPassword_WhenValid_ShouldReturn_True() throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         boolean result=user.validatePassword("Kpatil#123");
         Assert.assertTrue(result);
     }
     @Test
-    public void givenPassword_WhenValid_ShouldReturn_False() {
+    public void givenPassword_WhenValid_ShouldReturn_False() throws UserRegistrationException {
         UserRegistration user=new UserRegistration();
         boolean result=user.validatePassword("Kruti12");
         Assert.assertFalse(result);
